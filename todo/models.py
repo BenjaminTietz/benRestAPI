eede57d6f1ad6a1d2045fdf00c804a9b6f91e682
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-04 13:08:43 +0200

    feat: time_passed() implemented into models.py and serializers.py (TodoSerializer)
    A function time_passed() was implemented within the models. This calculates the past period in days between the creation of a todo and the current time. The function was inserted into the TodoSerializer in order to dynamically display the period between creation and the current day.

--- todo/models.py
+++ todo/models.py
@@ -1,13 +1,19 @@
 from django.db import models
-import datetime
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=30)
-    created_at = models.DateTimeField(default=datetime.date.today)
+    created_at = models.DateTimeField(default=datetime.now)
     user = models.ForeignKey(
         User, 
         on_delete=models.CASCADE,
         default=None)
+    
+    def time_passed(self):
+        today = datetime.now().date()
+        delta = today - self.created_at.date()
+
+        return delta.days
--- todo/serializers.py
+++ todo/serializers.py
@@ -2,7 +2,13 @@ from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 from .models import Todo
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Todo
-        fields = ['id','title', 'description', 'created_at'] #, 'user'
+        fields = ['id','title', 'description', 'created_at', 'user', 'time_passed'] 
